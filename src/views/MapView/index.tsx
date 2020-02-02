82dd6e4a4ffd4a934438e2383156ebf537509e47
progressivedev <zigmunds.laposko@gmail.com>
2020-02-02 19:22:14 +0200

    boilerplate map view

---
+++ src/views/MapView/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const MapView: React.FC = () => {
+    return (
+        <View>
+            <Text>Map</Text>
+        </View>
+    );
+};
---
+++ storybook/stories/views/MapView.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { MapView } from '../../../src/views/MapView';
+
+storiesOf('MapView', module).add('list', () => <MapView />);
