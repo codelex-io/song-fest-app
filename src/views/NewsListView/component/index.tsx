97eacad99854a6d574435e34d206795c4f122be0
Liva <liva.zvirgzdina@gmail.com>
2020-02-05 23:10:52 +0200

    for eslint

--- src/views/NewsListView/component/index.tsx
+++ src/views/NewsListView/component/index.tsx
@@ -1,32 +1,24 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { NewsItem } from '../types';
 import { FlatList, View } from 'react-native';
 import { colors } from '@styles';
 import { Card } from './Card';
-import { Header } from '@components';
-import { LocalizationContext } from '../../../localization/LocalizationContext';
 
-interface NewsListProps {
+interface Props {
     loading: boolean;
     items: NewsItem[];
 }
 
-const NewsListView: React.FC<NewsListProps> = ({ items }) => {
-    const { translations } = useContext(LocalizationContext);
-    return (
-        <View>
-            <Header title={translations.NEWS} />
-            <FlatList<NewsItem>
-                data={items}
-                renderItem={({ item, index }): React.ReactElement => (
-                    <View>
-                        <Card item={item} backgroundColor={colors.findColorByIndex(index)} />
-                    </View>
-                )}
-                ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
-            />
-        </View>
-    );
-};
+const NewsListView: React.FC<Props> = ({ items }) => (
+    <FlatList<NewsItem>
+        data={items}
+        renderItem={({ item, index }): React.ReactElement => (
+            <View>
+                <Card item={item} backgroundColor={colors.findColorByIndex(index)} />
+            </View>
+        )}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
+    />
+);
 
 export default NewsListView;
