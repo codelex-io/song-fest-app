9824f6846b440cc4cb13b79ede3e6f11a8b9d6cc
ProgressiveDev <zigmunds.laposko@gmail.com>
2020-02-18 11:41:43 +0200

    resolved merge conflicts

--- src/views/EventMapView/component/SearchBar.tsx
+++ src/views/EventMapView/component/SearchBar.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { View, TextInput, StyleSheet, Dimensions } from 'react-native';
 import { Icon, IconType } from '@components';
-<<<<<<< HEAD
-import { colors, typography } from '@styles';
-=======
 import { colors } from '@styles';
->>>>>>> master
 
 const width = Dimensions.get('window').width;
 
@@ -13,11 +9,6 @@ export const SearchBar = () => {
     return (
         <View style={styles.container}>
             <View style={styles.iconContainer}>
-<<<<<<< HEAD
-                <Icon size={25} type={IconType.Search} fill={'white'} />
-            </View>
-            <TextInput style={styles.input} placeholderTextColor="white" placeholder="MEKLĒŠANA" />
-=======
                 <Icon size={35} type={IconType.Search} fill={'white'} />
             </View>
             <TextInput
@@ -25,7 +16,6 @@ export const SearchBar = () => {
                 placeholderTextColor="white"
                 placeholder="Meklēt pēc nosaukuma, vietas uc."
             />
->>>>>>> master
         </View>
     );
 };
@@ -35,28 +25,11 @@ const styles = StyleSheet.create({
         width: width - 32,
         backgroundColor: colors.green,
         display: 'flex',
-<<<<<<< HEAD
-        alignItems: 'flex-start',
-=======
->>>>>>> master
         flexDirection: 'row',
         alignSelf: 'center',
         zIndex: 99,
         marginVertical: 16,
         position: 'absolute',
-<<<<<<< HEAD
-        paddingHorizontal: 15,
-    },
-    input: {
-        width: '85%',
-        fontSize: 16,
-        color: 'white',
-        fontFamily: typography.bold,
-    },
-    iconContainer: {
-        alignSelf: 'center',
-        marginRight: 11,
-=======
         top: 8,
     },
     iconContainer: {
@@ -72,6 +45,5 @@ const styles = StyleSheet.create({
         color: 'white',
         marginRight: 20,
         textTransform: 'uppercase',
->>>>>>> master
     },
 });
