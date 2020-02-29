5c3cc910d9e83691b86d732a91dfde13d69ae994
andrisLaduzans <andris.laduzans@gmail.com>
2020-02-29 23:12:22 +0200

    added external linking support functionality to MoreView buttons

--- src/views/MoreView/content.ts
+++ src/views/MoreView/content.ts
@@ -1,5 +1,6 @@
 import { IconType } from '@components/Icon';
 import { colors } from '@styles';
+import { Linking } from 'react-native';
 
 export interface ItemType {
     id: number;
@@ -9,12 +10,22 @@ export interface ItemType {
     onOpen: (navigate: (route: string) => void) => void;
 }
 
-const execute = (route: string, isInternal: boolean, navigate: (route: string) => void) => {
-    if (isInternal) {
+type TypeOfLink = 'external' | 'internal';
+
+const execute = (route: string, typeOfLink: TypeOfLink, navigate: (route: string) => void) => {
+    if (typeOfLink === 'internal') {
         navigate(route);
         console.log('navigating to', route);
     } else {
-        console.log('execute open browser');
+        Linking.canOpenURL(route)
+            .then(supported => {
+                if (!supported) {
+                    console.log("Can't handle url: " + route);
+                } else {
+                    return Linking.openURL(route);
+                }
+            })
+            .catch(err => console.error('An error occurred', err));
     }
 };
 
@@ -24,76 +35,81 @@ export const Items: ItemType[] = [
         title: 'Sponsoru Piedāvājums',
         icon: IconType.Gift,
         backgroundColor: colors.green,
-        onOpen: (navigate: (route: string) => void) => execute('News', true, navigate),
+        onOpen: (navigate: (route: string) => void) =>
+            execute(
+                'https://www.google.com/search?q=sponsoru+pied%C4%81v%C4%81jums&oq=sponso&aqs=chrome.0.69i59j69i57j0l5j69i60.1224j0j4&sourceid=chrome&ie=UTF-8',
+                'external',
+                navigate,
+            ),
     },
     {
         id: 1,
         title: 'Svarīgi \n zināt',
         icon: IconType.Alert,
         backgroundColor: colors.blue,
-        onOpen: (navigate: (route: string) => void) => execute('Events', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Events', 'internal', navigate),
     },
     {
         id: 2,
         title: 'Biežākie jautājumi',
         icon: IconType.HelpCircle,
         backgroundColor: colors.lightBlue,
-        onOpen: (navigate: (route: string) => void) => execute('Map', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Map', 'internal', navigate),
     },
     {
         id: 3,
         title: 'Transports pasākumā',
         icon: IconType.Bus,
         backgroundColor: colors.purple,
-        onOpen: (navigate: (route: string) => void) => execute('Video', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Video', 'internal', navigate),
     },
     {
         id: 4,
         title: 'Svētku info centrs',
         icon: IconType.Information,
         backgroundColor: colors.orange,
-        onOpen: (navigate: (route: string) => void) => execute('More', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('More', 'internal', navigate),
     },
     {
         id: 5,
         title: 'Akcija \n Zaļā pēda',
         icon: IconType.PineTreeBox,
         backgroundColor: colors.green,
-        onOpen: (navigate: (route: string) => void) => execute('Favorites', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Favorites', 'internal', navigate),
     },
     {
         id: 6,
         title: 'Svētku \n noteikumi',
         icon: IconType.FileDocumentBox,
         backgroundColor: colors.blue,
-        onOpen: (navigate: (route: string) => void) => execute('News', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('News', 'internal', navigate),
     },
     {
         id: 7,
         title: 'Informācija par kontaktiem',
         icon: IconType.Phone,
         backgroundColor: colors.lightBlue,
-        onOpen: (navigate: (route: string) => void) => execute('News', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('News', 'internal', navigate),
     },
     {
         id: 8,
         title: 'Lietotāja iestatījumi',
         icon: IconType.Settings,
         backgroundColor: colors.purple,
-        onOpen: (navigate: (route: string) => void) => execute('Language', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Language', 'internal', navigate),
     },
     {
         id: 9,
         title: 'Saturs\n vecākiem',
         icon: IconType.Parent,
         backgroundColor: colors.green,
-        onOpen: (navigate: (route: string) => void) => execute('Language', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Language', 'internal', navigate),
     },
     {
         id: 10,
         title: 'Valoda',
         icon: IconType.Settings,
         backgroundColor: colors.blue,
-        onOpen: (navigate: (route: string) => void) => execute('Language', true, navigate),
+        onOpen: (navigate: (route: string) => void) => execute('Language', 'internal', navigate),
     },
 ];
