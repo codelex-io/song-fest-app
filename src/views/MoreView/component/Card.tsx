6c107f6b858c557f4e66759a061b728d11f08c59
andrisLaduzans <andris.laduzans@gmail.com>
2020-02-29 22:29:04 +0200

    moreView media breakpoints for different devices

--- src/views/MoreView/component/Card.tsx
+++ src/views/MoreView/component/Card.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import { Text, StyleSheet, TouchableOpacity, Dimensions, View } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { IconType, Icon } from '@components';
 import { typography, colors } from '@styles';
+import { moreViewStyles } from './index';
 
 interface CardProps {
     title: string;
     icon: IconType;
-    backgroundColor: string;
     onOpen: () => void;
+    backgroundColor: string;
 }
 
 export const Card: React.FC<CardProps> = ({ title, icon, backgroundColor, onOpen }) => {
-    const screenWidth = Math.floor(Dimensions.get('window').width);
-    const btnWidth = Math.floor((screenWidth - 64) / 3);
     return (
-        <TouchableOpacity style={[styles.container, { width: btnWidth }]} onPress={onOpen}>
-            <View style={[styles.icon, { backgroundColor }]}>
-                <Icon type={icon} fill={colors.white} />
-            </View>
-            <Text style={styles.text}> {title}</Text>
-        </TouchableOpacity>
+        <View style={moreViewStyles.card}>
+            <TouchableOpacity style={moreViewStyles.button} onPress={onOpen}>
+                <View style={[styles.icon, { backgroundColor }]}>
+                    <Icon type={icon} fill={colors.white} />
+                </View>
+                <Text style={[styles.text]}> {title}</Text>
+            </TouchableOpacity>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 8,
-        alignItems: 'center',
-        marginVertical: 12,
-    },
     icon: {
         padding: 10,
     },
--- src/views/MoreView/component/index.tsx
+++ src/views/MoreView/component/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Dimensions, Text } from 'react-native';
 import { Card } from './Card';
 import { colors } from '@styles';
 import { Items, ItemType } from '../content';
+import { styles } from 'react-native-markdown-display';
 
 interface Props {
     navigate: (route: string) => void;
@@ -10,23 +11,41 @@ interface Props {
 
 const MoreView: React.FC<Props> = ({ navigate }) => {
     return (
-        <ScrollView style={styles.container}>
-            <View style={styles.inner}>
-                {Items.map((item: ItemType) => (
-                    <Card
-                        key={item.id}
-                        title={item.title}
-                        icon={item.icon}
-                        backgroundColor={item.backgroundColor}
-                        onOpen={() => item.onOpen(navigate)}
-                    />
+        <ScrollView style={moreViewStyles.container}>
+            <View style={moreViewStyles.inner}>
+                {Items.map((item: ItemType, id: number) => (
+                    <>
+                        <Card
+                            key={item.id}
+                            title={item.title}
+                            icon={item.icon}
+                            backgroundColor={item.backgroundColor}
+                            onOpen={() => item.onOpen(navigate)}
+                        />
+                        {id === Items.length ? (
+                            <View key={'stretcher'} style={styles.last}>
+                                <Text>last</Text>
+                            </View>
+                        ) : null}
+                    </>
                 ))}
             </View>
         </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({
+const screenWidth = Math.floor(Dimensions.get('window').width);
+
+let columnCount = 3;
+if (screenWidth < 360) {
+    columnCount = 2;
+} else if (screenWidth > 600) {
+    columnCount = 5;
+}
+
+const cardWidth = Math.floor((screenWidth - 32) / columnCount);
+
+export const moreViewStyles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.white,
@@ -35,7 +54,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         paddingTop: 4,
-        paddingHorizontal: 8,
+        paddingHorizontal: 16,
+    },
+    card: {
+        alignItems: 'center',
+        marginVertical: 12,
+        width: cardWidth,
+    },
+    button: {
+        width: 99,
+        alignItems: 'center',
     },
 });
 
