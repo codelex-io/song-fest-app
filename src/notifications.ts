04d0eed76a68e711e8529b264f21f8a3e78b18ef
sandris <sandris.artemjevs@codelex.io>
2020-03-02 13:25:52 +0200

    minor cleanup

--- src/notifications.ts
+++ src/notifications.ts
@@ -1,45 +1,51 @@
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-community/async-storage';
 import firebase from 'react-native-firebase';
-//...
-const getToken = async ():Promise<void> => {
-    let fcmToken = await AsyncStorage.getItem('fcmToken');
-    
+import { errors } from '@utils';
+
+const KEY = 'fcmToken';
+
+const getToken = async (): Promise<void> => {
+    let fcmToken = await AsyncStorage.getItem(KEY);
     if (!fcmToken) {
         fcmToken = await firebase.messaging().getToken();
         if (fcmToken) {
-            await AsyncStorage.setItem('fcmToken', fcmToken);
+            await AsyncStorage.setItem(KEY, fcmToken);
         }
     }
-}
+};
 
-const checkPermission = async() => {
+const checkPermission = async () => {
     const enabled = await firebase.messaging().hasPermission();
     if (enabled) {
         getToken();
     } else {
         requestPermission();
     }
-}
+};
 
-const requestPermission = async() => {
+const requestPermission = async () => {
     try {
         await firebase.messaging().requestPermission();
         getToken();
     } catch (error) {
-        console.log('permission rejected');
+        errors.onError(error);
     }
-}
+};
 
-const createNotificationListeners = async() => {
+const createNotificationListeners = async () => {
     firebase.notifications().onNotification(notification => {
-        notification.android.setChannelId('insider').setSound('default')
-        firebase.notifications().displayNotification(notification)
+        notification.android.setChannelId('insider').setSound('default');
+        firebase.notifications().displayNotification(notification);
     });
-}
+};
 
-export const init = async() => {
-    const channel = new firebase.notifications.Android.Channel('insider', 'insider channel', firebase.notifications.Android.Importance.Max)
+export const init = async () => {
+    const channel = new firebase.notifications.Android.Channel(
+        'insider',
+        'insider channel',
+        firebase.notifications.Android.Importance.Max,
+    );
     await firebase.notifications().android.createChannel(channel);
     await checkPermission();
     await createNotificationListeners();
-}
+};
