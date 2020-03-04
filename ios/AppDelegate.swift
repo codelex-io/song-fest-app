83125082ebec26be314f4b04b5edc305e4c47378
sandris <a.sandris@gmail.com>
2020-03-04 09:46:15 +0200

    splash screen

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -7,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RCTBridgeDelegate {
   var window: UIWindow?
   var bridge: RCTBridge!
   static var reactView: RCTRootView!
-  static var instance: AppDelegate = AppDelegate.init()
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -28,27 +27,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RCTBridgeDelegate {
   
   func showLoading() -> Void {
     let controller = UIStoryboard(name: "Main", bundle: nil)
-      .instantiateViewController(withIdentifier: "GifViewController") as! GifViewController
+      .instantiateViewController(withIdentifier: "PostLaunchController") as! PostLaunchController
 
     self.window?.rootViewController = controller
     self.window?.makeKeyAndVisible()
   }
     
-  // instead of this method, notify swift app (maybe via variable reactInitiated in AppDelegate with a listener)
-//  static var reactAppInitiated: Bool = false
   @objc
   func showReactApp() -> Void {
-    print("react app would be initiated from App.tsx")
-    // maybe
-    // self.reactAppInitiated = true
-    
-//    DispatchQueue.main.async {
-//      let controller = UIViewController()
-//      controller.view = AppDelegate.reactView
-//
-//      UIApplication.shared.windows.first?.rootViewController = controller
-//      UIApplication.shared.windows.first?.makeKeyAndVisible()
-//    }
+    DispatchQueue.main.async {
+      let controller = UIViewController()
+      controller.view = AppDelegate.reactView
+
+      UIApplication.shared.windows.first?.rootViewController = controller
+      UIApplication.shared.windows.first?.makeKeyAndVisible()
+    }
   }
   
   func sourceURL(for bridge: RCTBridge!) -> URL! {
---
+++ ios/PostLaunchController.swift
@@ -0,0 +1,34 @@
+import Foundation
+import TinyConstraints
+import UIKit
+import Gifu
+
+class PostLaunchController: UIViewController {
+
+  lazy var loadinggif: GIFImageView = {
+    let view = GIFImageView()
+    view.contentMode = .scaleAspectFit
+    view.animate(withGIFNamed: "songfest-launch-gif")
+    view.alpha = 0.0
+    return view
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.addSubview(loadinggif)
+    loadinggif.centerInSuperview()
+    loadinggif.width(view.frame.width * 0.9)
+
+    // fade in/out animation for gif
+    UIView.animate(
+      withDuration: 1.55,
+      delay: 0.8,
+      options: [.curveEaseOut, .autoreverse, .repeat],
+      animations: {
+        self.loadinggif.alpha = 1.0
+      },
+      completion: nil
+    )
+  }
+}
