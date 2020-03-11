5a9d5831f298d012af041a49f1ad0940aef53bb5
Liva <liva.zvirgzdina@gmail.com>
2020-03-11 13:34:10 +0200

    LanguageViewStart to LanguageView

--- storybook/stories/storyLoader.ts
+++ storybook/stories/storyLoader.ts
@@ -17,4 +17,4 @@ import './views/VideoView.stories';
 import './views/EventMapView.stories';
 import './views/LoadingView.stories';
 import './views/EmptyView.stories';
-import './views/LanguageViewStart';
+import './views/LanguageView.stories';
