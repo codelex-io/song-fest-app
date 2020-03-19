1cf5b1c6ef666aad2b8c76a4ded88f04b7f46871
andrisLaduzans <andris.laduzans@gmail.com>
2020-03-19 20:43:51 +0200

    more performant version

--- src/components/layers/FeedLayout.tsx
+++ src/components/layers/FeedLayout.tsx
@@ -1,64 +1,78 @@
-import React, { ReactNode } from 'react';
-import { StyleSheet, StatusBar, View } from 'react-native';
+import React, { ReactNode, useState } from 'react';
+import { StyleSheet, StatusBar, LayoutChangeEvent } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { Loading } from '@components';
 
 interface FeedLayerProps {
-    header: ReactNode;
-    children: ReactNode | ReactNode[];
-    animatedScrollOffset: Animated.Value<0>;
-    loading: boolean;
+   header: ReactNode;
+   children: ReactNode | ReactNode[];
+   animatedScrollOffset: Animated.Value<0>;
+   loading: boolean;
 }
 
 const FeedLayout: React.FC<FeedLayerProps> = ({ header, children, animatedScrollOffset, loading }) => {
-    const diffClampScrollY = Animated.diffClamp(animatedScrollOffset, 0, 126);
+   const [headerHeight, setHeaderHeight] = useState<number | undefined>(undefined);
 
-    const headerInterpolation = Animated.interpolate(diffClampScrollY, {
-        inputRange: [0, 126],
-        outputRange: [0, -126],
-    });
+   const diffClampScrollY = Animated.diffClamp(animatedScrollOffset, 0, headerHeight ? headerHeight : 126);
 
-    const contentInterpolation = Animated.interpolate(diffClampScrollY, {
-        inputRange: [0, 126],
-        outputRange: [126, 0],
-    });
+   const headerInterpolation = Animated.interpolate(diffClampScrollY, {
+      inputRange: [0, headerHeight ? headerHeight : 126],
+      outputRange: [0, headerHeight ? -headerHeight : -126],
+   });
 
-    const animatedHeaderStyles = {
-        transform: [{ translateY: headerInterpolation }],
-    };
+   const contentInterpolation = Animated.interpolate(diffClampScrollY, {
+      inputRange: [0, headerHeight ? headerHeight : 126],
+      outputRange: [headerHeight ? headerHeight : 126, 0],
+   });
 
-    const animatedContentStyles = {
-        paddingTop: contentInterpolation,
-    };
+   const animatedHeaderStyles = {
+      transform: [{ translateY: headerInterpolation }],
+   };
 
-    return (
-        <Animated.View style={[styles.container, animatedContentStyles]}>
-            <StatusBar />
+   const animatedContentStyles = {
+      transform: [{ translateY: contentInterpolation }],
+   };
 
-            <Animated.View style={[styles.header, animatedHeaderStyles]}>{header}</Animated.View>
+   const measureHeader = (event: LayoutChangeEvent) => {
+      const { height } = event.nativeEvent.layout;
+      setHeaderHeight(height);
+   };
 
-            {loading ? <Loading /> : <View style={styles.content}>{children}</View>}
-        </Animated.View>
-    );
+   return (
+      <Animated.View style={[styles.container]}>
+         <StatusBar />
+
+         <Animated.View onLayout={event => measureHeader(event)} style={[styles.header, animatedHeaderStyles]}>
+            {header}
+         </Animated.View>
+
+         {loading ? (
+            <Loading />
+         ) : (
+               <Animated.View style={[styles.content, animatedContentStyles]}>{children}</Animated.View>
+            )}
+      </Animated.View>
+   );
 };
 
 export default FeedLayout;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1,
-        backgroundColor: '#fff',
-    },
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+   container: {
+      flex: 1,
+      overflow: 'hidden',
+   },
+   header: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      zIndex: 1,
+      backgroundColor: '#fff',
+   },
+   content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
 });
