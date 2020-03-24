a3feb1c8640a1a38c809745a97ee29084b71639a
Andris Laduzans <51923975+andrisLaduzans@users.noreply.github.com>
2020-03-24 18:57:58 +0200

    Update FeedLayout.tsx

--- src/components/layers/FeedLayout.tsx
+++ src/components/layers/FeedLayout.tsx
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         zIndex: 1,
-        backgroundColor: colors.purple,
+        backgroundColor: colors.white,
     },
     content: {
         flex: 1,
