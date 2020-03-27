bb3ae2d3a25ecbb8a3b8f12251731b61f90a15fc
sandris <sandris.artemjevs@codelex.io>
2020-03-27 21:15:02 +0200

    android location prompt

--- src/domain/location/index.ts
+++ src/domain/location/index.ts
@@ -11,25 +11,31 @@ const onError = (msg: ErrorMessage) => {
     Alert.alert(msg.title, msg.details, [{ text: 'OK' }], { cancelable: false });
 };
 
-export const getCurrentPosition = (msg: ErrorMessage): Promise<Coordinates> => {
-    return new Promise<Coordinates>(resolve => {
-        Geolocation.getCurrentPosition(
-            position => {
-                resolve({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-            },
-            () => {
-                if (Platform.OS === 'android') {
-                    PermissionsAndroid.request('android.permission.ACCESS_FINE_LOCATION').catch(e => {
-                        // eslint-disable-next-line no-console
-                        console.error(e);
-                        onError(msg);
+export const getCurrentPosition = (msg: ErrorMessage): Promise<Coordinates | undefined> => {
+    const doGetPosition = (): Promise<Coordinates> =>
+        new Promise<Coordinates>((resolve, reject) => {
+            Geolocation.getCurrentPosition(
+                position => {
+                    resolve({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
                     });
-                }
-            },
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
-        );
+                },
+                reject,
+                { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+            );
+        });
+
+    return doGetPosition().catch(() => {
+        if (Platform.OS === 'android') {
+            return PermissionsAndroid.request('android.permission.ACCESS_FINE_LOCATION')
+                .then(doGetPosition)
+                .catch(e => {
+                    // eslint-disable-next-line no-console
+                    console.error(e);
+                    onError(msg);
+                    return undefined;
+                });
+        }
     });
 };
